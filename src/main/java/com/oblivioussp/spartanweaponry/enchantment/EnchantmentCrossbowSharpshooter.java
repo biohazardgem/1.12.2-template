c55f41399216a96ca882d5c2fb8e4b9df73ad32d
biohazardgem <biohazardnuke55@gmail.com>
2025-04-12 13:14:19 -0400

    Add files via upload

---
+++ src/main/java/com/oblivioussp/spartanweaponry/enchantment/EnchantmentCrossbowSharpshooter.java
@@ -0,0 +1,31 @@
+package com.oblivioussp.spartanweaponry.enchantment;
+
+import net.minecraft.inventory.EntityEquipmentSlot;
+
+public class EnchantmentCrossbowSharpshooter extends EnchantmentSW 
+{
+
+	public EnchantmentCrossbowSharpshooter(Rarity rarityIn) 
+	{
+		super("sharpshooter", rarityIn, TYPE_CROSSBOW, new EntityEquipmentSlot[]{EntityEquipmentSlot.MAINHAND, EntityEquipmentSlot.OFFHAND});
+	}
+	
+	@Override
+	public int getMinEnchantability(int enchantmentLevel) 
+	{
+		return 15 + (enchantmentLevel - 1) * 10;
+	}
+	
+	@Override
+	public int getMaxEnchantability(int enchantmentLevel) 
+	{
+		return 50;
+	}
+	
+	@Override
+	public int getMaxLevel()
+	{
+		return 3;
+	}
+
+}
