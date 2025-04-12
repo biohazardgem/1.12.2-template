1449899771974fcf5c005481f0c4b21531746576
biohazardgem <biohazardnuke55@gmail.com>
2025-04-12 13:18:06 -0400

    Add files via upload

---
+++ src/main/java/com/oblivioussp/spartanweaponry/util/ExplosionHelper.java
@@ -0,0 +1,17 @@
+package com.oblivioussp.spartanweaponry.util;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.BlockPos;
+
+public class ExplosionHelper
+{
+	public static void explode(Entity explosive, Entity exploder, BlockPos blockPos)
+	{
+		if(!explosive.world.isRemote)
+		{
+			boolean mobGriefing = explosive.world.getGameRules().getBoolean("mobGriefing");
+			explosive.world.createExplosion(explosive, blockPos.getX() + 0.5d, blockPos.getY() + 0.5d, blockPos.getZ() + 0.5d, ConfigHandler.explosionStrengthArrowExplosive, ConfigHandler.enableTerrainDamage && mobGriefing);
+			explosive.setDead();
+		}
+	}
+}
