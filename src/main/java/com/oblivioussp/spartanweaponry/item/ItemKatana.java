19fffe7f910b52c86df924f6acdfdaf32da9fb5f
biohazardgem <biohazardnuke55@gmail.com>
2025-04-12 13:16:39 -0400

    Add files via upload

---
+++ src/main/java/com/oblivioussp/spartanweaponry/item/ItemKatana.java
@@ -0,0 +1,30 @@
+package com.oblivioussp.spartanweaponry.item;
+
+import com.oblivioussp.spartanweaponry.api.ToolMaterialEx;
+import com.oblivioussp.spartanweaponry.api.WeaponProperties;
+import com.oblivioussp.spartanweaponry.util.ConfigHandler;
+
+public class ItemKatana extends ItemSwordBase
+{
+	public ItemKatana(String unlocName, ToolMaterialEx material) 
+	{
+		super(unlocName, material, ConfigHandler.damageBaseKatana, ConfigHandler.damageMultiplierKatana, ConfigHandler.speedKatana, WeaponProperties.TWO_HANDED_1,
+				WeaponProperties.SWEEP_DAMAGE_NORMAL, WeaponProperties.EXTRA_DAMAGE_2_CHEST);
+		displayName = "katana_custom";
+	}
+	
+	public ItemKatana(String unlocName, String externalModId, ToolMaterialEx material) 
+	{
+		this(unlocName, material);
+		modId = externalModId;
+	}
+	
+	/**
+	 * @deprecated This old constructor is only here for compatibility reasons. Update your addon mods instead of using this method.
+	 */
+	@Deprecated
+	public ItemKatana(String unlocName, String externalModId, ToolMaterialEx material, float weaponDamage) 
+	{
+		this(unlocName, externalModId, material);
+	}
+}
---
+++ src/main/java/com/oblivioussp/spartanweaponry/item/ItemLongsword.java
@@ -0,0 +1,29 @@
+package com.oblivioussp.spartanweaponry.item;
+
+import com.oblivioussp.spartanweaponry.api.ToolMaterialEx;
+import com.oblivioussp.spartanweaponry.api.WeaponProperties;
+import com.oblivioussp.spartanweaponry.util.ConfigHandler;
+
+public class ItemLongsword extends ItemSwordBase
+{
+	public ItemLongsword(String unlocName, ToolMaterialEx material)
+	{
+		super(unlocName, material, ConfigHandler.damageBaseLongsword, ConfigHandler.damageMultiplierLongsword, ConfigHandler.speedLongsword, WeaponProperties.TWO_HANDED_1, WeaponProperties.SWEEP_DAMAGE_NORMAL);
+		displayName = "longsword_custom";
+	}
+	
+	public ItemLongsword(String unlocName, String externalModId, ToolMaterialEx material)
+	{
+		this(unlocName, material);
+		modId = externalModId;
+	}
+
+	/**
+	 * @deprecated This old constructor is only here for compatibility reasons. Update your addon mods instead of using this method.
+	 */
+	@Deprecated
+	public ItemLongsword(String unlocName, String externalModId, ToolMaterialEx material, float weaponDamage) 
+	{
+		this(unlocName, externalModId, material);
+	}
+}
